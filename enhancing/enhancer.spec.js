9ba4e3fac5085fea7f2fc19332a9cf2a0a48fc06
emma <sonicjane2020@gmail.com>
2020-04-23 01:09:02 -0700

    mvp

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,88 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { repair, succeed, fail, get } = require("./enhancer.js");
+
+describe("enhancer.js", () => {
+  describe("repair()", () => {
+    it("takes item, changes durability to 100", () => {
+      const item = {
+        name: "emma",
+        durability: 10,
+        enhancement: 15
+      };
+      //arrange
+      const expected = {
+        name: "emma",
+        durability: 100,
+        enhancement: 15
+      };
+
+      //act
+      const repairedItem = repair(item);
+
+      //assert
+      expect(repairedItem).toEqual(expected);
+    });
+  });
+
+  describe("succeed()", () => {
+    it("raises enhancement", () => {
+      const item = {
+        durability: 10,
+        enhancement: 15
+      };
+      const expected = {
+        durability: 100,
+        enhancement: 16
+      };
+
+      const enhancedItem = succeed(item);
+      expect(enhancedItem).toEqual(expected);
+    });
+  });
+
+  describe("fail()", () => {
+    it("decreases durability", () => {
+      const item = {
+        durability: 50,
+        enhancement: 14
+      };
+
+      const expected = {
+        durability: 45,
+        enhancement: 14
+      };
+
+      const failItem = fail(item);
+      expect(failItem).toEqual(expected);
+    });
+
+    it("decreases durability", () => {
+      const item = {
+        durability: 50,
+        enhancement: 16
+      };
+
+      const expected = {
+        durability: 40,
+        enhancement: 16
+      };
+
+      const failItem = fail(item);
+      expect(failItem).toEqual(expected);
+    });
+
+    it("decreases durability", () => {
+      const item = {
+        durability: 50,
+        enhancement: 18
+      };
+
+      const expected = {
+        durability: 40,
+        enhancement: 17
+      };
+
+      const failItem = fail(item);
+      expect(failItem).toEqual(expected);
+    });
+  });
+});
